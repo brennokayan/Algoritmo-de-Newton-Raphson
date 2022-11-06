1193d5ab8028d7156f57f3073dfccc85c2e3ccd5
brennokayan <brennokayan@hotmail.com>
2022-11-05 21:45:12 -0400

    a

--- Newton Raphson.py
+++ Newton Raphson.py
@@ -1,1 +1,15 @@
+import math as mt
+import numpy as np
 from sympy import *
+e = 2.71828182846
+x, y, z = symbols('x y z')
+valor = 3
+
+func = np.log(valor)
+
+
+derivada_de_funcao = diff(ln(x) , x)
+derivada_de_funcao = der
+soma = valor - func / derivada_de_funcao
+print(func,"\n",derivada_de_funcao)
+print(soma)
