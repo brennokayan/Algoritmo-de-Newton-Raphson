f74d6558d53c8135c7cc44b85b1660f4b91dc8c3
brennokayan <brennokayan@hotmail.com>
2022-11-16 02:10:26 -0400

    Update Newton Raphson.py

--- Newton Raphson.py
+++ Newton Raphson.py
@@ -1,15 +1,15 @@
 import math as mt
 import numpy as np
-from sympy import *
+import sympy as sp
 e = 2.71828182846
-x, y, z = symbols('x y z')
+x, y, z = sp.symbols('x y z')
 valor = 3
 
 func = np.log(valor)
 
 
-derivada_de_funcao = diff(ln(x) , x)
-derivada_de_funcao = der
+derivada_de_funcao = sp.diff(sp.ln(x) , x)
+a = derivada_de_funcao.split()
 soma = valor - func / derivada_de_funcao
 print(func,"\n",derivada_de_funcao)
 print(soma)
